b636f2c13daa61ee73869dcb72b327baa1f72f52
Yash Saxena <yashsaxena933@gmail.com>
2025-08-01 17:18:46 +0530

    commit

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import Router from 'express';
-import {registerUser,loginUser,logoutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateAccountDetails,updateUserAvatar,updateUserCoverImage,getWatchHistory} from '../controllers/user.controller.js';
+import {registerUser,loginUser,logoutUser,refreshAccessToken,getUserChannelProfile,changeCurrentPassword,getCurrentUser,updateAccountDetails,updateUserAvatar,updateUserCoverImage,getWatchHistory} from '../controllers/user.controller.js';
 import { upload } from '../middlewares/multer.middleware.js';
 import { verifyJWT } from '../middlewares/auth.middleware.js';
 const router =Router();
